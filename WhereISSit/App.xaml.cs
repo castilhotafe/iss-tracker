f386e1442b16709e2d4396522acca300056b9d96
Marcos  Castilho <marcosalmaa@icloud.com>
2025-10-04 10:37:27 +0800

    Initial commit

---
+++ WhereISSit/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WhereISSit;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WhereISSit/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WhereISSit;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
