96444f67ff26bd9288a84b91ad6653c341cfef0d
Marcos  Castilho <marcosalmaa@icloud.com>
2025-10-07 00:50:41 +0800

    feat:first attempt on creating a map for the stalking page

--- WhereISSit/MauiProgram.cs
+++ WhereISSit/MauiProgram.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Maps; // Necessário para o controle <maps:Map>
 
 namespace WhereISSit;
 
@@ -8,9 +12,11 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
         builder
             .UseMauiApp<App>()
             .UseMauiCommunityToolkit()
+            .UseMauiMaps() // Ativa o suporte a mapas no MAUI
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
