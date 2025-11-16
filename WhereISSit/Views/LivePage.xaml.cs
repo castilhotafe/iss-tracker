224d02d37853882738e523823abfc3c9133c55ee
Marcos  Castilho <>
2025-11-16 23:33:53 +0800

    feat: added LivePage and updated AppShell / StalkingPage

---
+++ WhereISSit/Views/LivePage.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace WhereISSit.Views
+{
+    public partial class LivePage : ContentPage
+    {
+        public LivePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
